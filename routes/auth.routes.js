c9b2e89b8363a9379aec9ee2a83a86e7ad024288
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2024-08-30 14:22:45 +0200

    feat: (auth) implement login

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,6 @@
 
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const router = require("express").Router();
 
@@ -71,11 +72,63 @@ router.post('/signup', (req, res, next) => {
       res.status(201).json({ user: user });
     })
     .catch(err => {
-      console.log(err);
+      console.log("Error creating an account...", err);
       res.status(500).json({ message: "Internal Server Error" })
     });
 });
 
+
+// POST /auth/login - Verifies email and password and returns a JWT
+router.post('/login', (req, res, next) => {
+
+  const { email, password } = req.body;
+
+  // Check if email or password are provided as empty string 
+  if ( !email || !password ) {
+    res.status(400).json({ message: "Provide email and password." });
+    return;
+  }
+
+  // Check the users collection if a user with the same email exists
+  User.findOne({ email })
+    .then((foundUser) => {
+
+      if (!foundUser) {
+        // If the user is not found, send an error response
+        res.status(401).json({ message: "User not found." })
+        return;
+      }
+
+      // Compare the provided password with the one saved in the database
+      const passwordCorrect = bcrypt.compareSync(password, foundUser.password);
+
+      if (passwordCorrect) {
+        // Deconstruct the user object to omit the password
+        const { _id, email, name } = foundUser;
+
+        // Create an object that will be set as the token payload
+        const payload = { _id, email, name };
+
+        // Create and sign the token
+        const authToken = jwt.sign(
+          payload,
+          process.env.TOKEN_SECRET,
+          { algorithm: 'HS256', expiresIn: "6h" }
+        );
+
+        // Send the token as the response
+        res.status(200).json({ authToken: authToken });
+      }
+      else {
+        res.status(401).json({ message: "Unable to authenticate the user" });
+      }
+
+    })
+    .catch(err => {
+      console.log("Error trying to login...", err);
+      res.status(500).json({ message: "Internal Server Error" })
+    });
+});
 
 
 module.exports = router;
